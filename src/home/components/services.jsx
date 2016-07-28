a96016ab445649a613aed4378cdbf563103b9f2d
dwtupper <dtupper@outlook.com>
2016-07-27 20:00:20 -0400

    Added Hover functionality to services graphics

--- src/home/components/services.jsx
+++ src/home/components/services.jsx
@@ -5,8 +5,22 @@ import MediaAssetPath from '../homeContainer';
 
 
 export default class Services extends Component  {
-  constructor() {
-    super();
+  render() {
+    return(
+      <div className='services'>
+        <WebServices />
+        <ProductConsulting />
+        <Button />
+        <MobileDevelopment />
+        <HostingManagement />
+      </div>
+    );
+  }
+}
+
+class WebServices extends Component {
+  constructor(props) {
+    super(props);
     this.state = {id: 'inactive'};
     this.mouseOver = this.mouseOver.bind(this)
     this.mouseOut = this.mouseOut.bind(this)
@@ -20,31 +34,120 @@ export default class Services extends Component  {
     this.setState({id: 'inactive'});
   }
 
+
   render() {
     return(
-      <div className='services'>
-        <div onMouseOver={this.mouseOver} onMouseOut={this.mouseOut} className="services_tile" id={this.state.id}>
-          <img id='services_img' src="src/assets/img/home/webdevServices.png"/>
-          <p id='services_text'>Web <br/> Development</p>
+      <div onMouseOver={this.mouseOver} onMouseOut={this.mouseOut} className="services_tile" id={this.state.id}>
+        <div>
+          <img id="image" src="src/assets/img/home/webdevServices.png"/>
         </div>
-        <div onMouseOver={this.mouseOver} onMouseOut={this.mouseOut} className="services_tile" id={this.state.id}>
-          <img id='services_img' src="src/assets/img/home/webdevServices.png"/>
-          <p id='services_text'>Web <br/> Development</p>
+        <div>
+          <p id="text">Web <br/> Development</p>
         </div>
-
-        <div className="services_tile">
-          <button>Button</button>
-        </div>
-        <div onMouseOver={this.mouseOver} onMouseOut={this.mouseOut} className="services_tile" id={this.state.id}>
-          <img id='services_img' src="src/assets/img/home/webdevServices.png"/>
-          <p id='services_text'>Web <br/> Development</p>
-        </div>
-        <div onMouseOver={this.mouseOver} onMouseOut={this.mouseOut} className="services_tile" id={this.state.id}>
-          <img id='services_img' src="src/assets/img/home/webdevServices.png"/>
-          <p id='services_text'>Web <br/> Development</p>
-        </div>
-
       </div>
-    );
+    )
+  }
+}
+
+class ProductConsulting extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {id: 'inactive'};
+    this.mouseOver = this.mouseOver.bind(this)
+    this.mouseOut = this.mouseOut.bind(this)
+  }
+
+  mouseOver() {
+      this.setState({id: 'active'});
+  }
+
+  mouseOut() {
+    this.setState({id: 'inactive'});
+  }
+
+
+  render() {
+    return(
+      <div onMouseOver={this.mouseOver} onMouseOut={this.mouseOut} className="services_tile" id={this.state.id}>
+        <div>
+          <img id="image" src="src/assets/img/home/productdevServices.png"/>
+        </div>
+        <div>
+          <p id="text">Product <br/> Consulting</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+class Button extends Component {
+  render() {
+    return(
+      <div className="services_tile">
+        <button id="servicesArrow" ><img src="src/assets/img/home/arrowButton.png"/></button>
+      </div>
+    )
+  }
+}
+
+class MobileDevelopment extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {id: 'inactive'};
+    this.mouseOver = this.mouseOver.bind(this)
+    this.mouseOut = this.mouseOut.bind(this)
+  }
+
+  mouseOver() {
+      this.setState({id: 'active'});
+  }
+
+  mouseOut() {
+    this.setState({id: 'inactive'});
+  }
+
+
+  render() {
+    return(
+      <div onMouseOver={this.mouseOver} onMouseOut={this.mouseOut} className="services_tile" id={this.state.id}>
+        <div>
+          <img id="image" src="src/assets/img/home/mobiledevServices.png"/>
+        </div>
+        <div>
+          <p id="text"> Mobile <br/> Development</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+class HostingManagement extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {id: 'inactive'};
+    this.mouseOver = this.mouseOver.bind(this)
+    this.mouseOut = this.mouseOut.bind(this)
+  }
+
+  mouseOver() {
+      this.setState({id: 'active'});
+  }
+
+  mouseOut() {
+    this.setState({id: 'inactive'});
+  }
+
+
+  render() {
+    return(
+      <div onMouseOver={this.mouseOver} onMouseOut={this.mouseOut} className="services_tile" id={this.state.id}>
+        <div>
+          <img id="image" src="src/assets/img/home/hostingServices.png"/>
+        </div>
+        <div>
+          <p id="text">Hosting <br/> Management</p>
+        </div>
+      </div>
+    )
   }
 }
